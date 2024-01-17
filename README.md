9d50172055699ad5aabc0870407f0eb53004c6b7
Fabiano <fabianosafe@gmail.com>
2024-01-17 16:36:00 -0300

    1

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
+Instalações
+Node
+
+
+
 # Supabase Next.js Auth & User Management Starter
+
 
 This example will set you up for a very common situation: users can sign up or sign in and then update their account with public profile information, including a profile image.
 
--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,7 @@ export default function Home() {
       <div className="col-6">
         <h1 className="header">Supabase Auth + Storage</h1>
         <p>
-          Experience our Auth and Storage through a simple profile management example. Create a user
-          profile and upload an avatar image. Fast, simple, secure.
+          Safe
         </p>
       </div>
       <div className="col-6 auth-widget">
